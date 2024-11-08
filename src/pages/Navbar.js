cceb8d7f11b2e9ba305d8bbae58a276bbfc8f5bc
lqasemzadeh <lqasemzadeh@gmail.com>
2024-11-08 07:53:54 +0100

    Add global CSS and update Navbar styles
    Imported a new global CSS file into the app and applied several styling updates to the Navbar component. Adjusted the layout and spacing for different screen sizes to improve responsiveness.

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from "next/link";
 import { HiMenuAlt3 } from "react-icons/hi";
-import {create as item} from "eslint-plugin-react/lib/rules/sort-prop-types";
+
 
 const listItems = [
     'Home',
@@ -15,14 +15,14 @@ const Navbar = () => {
     return (
 
         <div
-            className="fixed top-5 left-1/2 bg-white flex justify-between items-center gap-16 py-3 px-10
-            transform -translate-x-1/2 rounded-full bg-opacity-30 backdrop-blur-md text-black shadow-lg z-40
-             hidden md:flex">
+            className="fixed top-5 left-1/2 bg-white flex justify-between items-center gap-1 py-1 px-2
+    transform -translate-x-1/2 rounded-full bg-opacity-30 backdrop-blur-md text-black shadow-lg z-40
+     hidden md:flex sm:gap-2 sm:py-2 sm:px-5">
             <img src="/assets/Logo/logo-transparent-png.png" className="w-16 cursor-pointer"/>
-            <ul className="flex gap-8 text-xl font-semibold">
+            <ul className="flex space-x-4 text-md font-semibold sm:gap-4 sm:text-xl">
                 {
                     listItems.map((item, index) => (
-                        <li key={index} className="cursor-pointer text-sm">
+                        <li key={index} className="cursor-pointer text-sm sm:text-sm">
                             <Link href={`/${item.toLowerCase()}`}>
                                 <a>
                                     {item}
