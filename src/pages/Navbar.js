bfdbe413052fcb1bc9a2f7909fdbd47df5b31ff4
lqasemzadeh <lqasemzadeh@gmail.com>
2024-11-08 07:29:20 +0100

    Revise Navbar positioning and layout for small screens
    Updated the Navbar to be hidden on smaller screens and refined its positioning for better alignment on larger screens. The changes include centering the Navbar using `transform -translate-x-1/2` and making it fixed at the top with some margin. Added a media query class to hide the Navbar on smaller screens while ensuring a consistent design across different screen sizes.

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -13,26 +13,25 @@ const listItems = [
 
 const Navbar = () => {
     return (
-        <div className="mt-5">
-            <div className="fixed bg-white flex justify-between items-center gap-16
-         py-3 px-10 translate-x-[-50%] left-1/4 rounded-full
-         bg-opacity-30 text-black shadow-lg z-40">
-                <img src="/assets/Logo/logo-transparent-png.png" className="w-16 cursor-pointer"/>
-                <ul className="flex gap-8 text-xl font-semibold">
-                    {
-                        listItems.map((item, index) => (
-                            <li key={index} className="cursor-pointer text-sm">
-                                <Link href={`/${item.toLowerCase()}`}>
-                                    <a>
-                                        {item}
-                                    </a>
-                                </Link>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
+
+        <div
+            className="fixed top-5 left-1/2 bg-white flex justify-between items-center gap-16 py-3 px-10 transform -translate-x-1/2 rounded-full bg-opacity-30 text-black shadow-lg z-40 hidden md:flex">
+            <img src="/assets/Logo/logo-transparent-png.png" className="w-16 cursor-pointer"/>
+            <ul className="flex gap-8 text-xl font-semibold">
+                {
+                    listItems.map((item, index) => (
+                        <li key={index} className="cursor-pointer text-sm">
+                            <Link href={`/${item.toLowerCase()}`}>
+                                <a>
+                                    {item}
+                                </a>
+                            </Link>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
+
     );
 };
 
