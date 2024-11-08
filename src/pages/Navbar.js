bd2c92c2a083350f6e54c73b8bb0f1d9cf064ecb
lqasemzadeh <lqasemzadeh@gmail.com>
2024-11-08 07:34:30 +0100

    Add backdrop blur effect to Navbar component
    Implemented `backdrop-blur-md` to enhance the visual appeal of the Navbar. This change improves readability and aesthetics, especially when scrolled over varied backgrounds.

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -15,7 +15,9 @@ const Navbar = () => {
     return (
 
         <div
-            className="fixed top-5 left-1/2 bg-white flex justify-between items-center gap-16 py-3 px-10 transform -translate-x-1/2 rounded-full bg-opacity-30 text-black shadow-lg z-40 hidden md:flex">
+            className="fixed top-5 left-1/2 bg-white flex justify-between items-center gap-16 py-3 px-10
+            transform -translate-x-1/2 rounded-full bg-opacity-30 backdrop-blur-md text-black shadow-lg z-40
+             hidden md:flex">
             <img src="/assets/Logo/logo-transparent-png.png" className="w-16 cursor-pointer"/>
             <ul className="flex gap-8 text-xl font-semibold">
                 {
