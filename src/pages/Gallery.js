b927e19d81985d305855d802b08dcfaa850440a1
lqasemzadeh <lqasemzadeh@gmail.com>
2024-11-23 19:59:34 +0100

    Update Gallery layout to a two-column grid
    Revised the Gallery component to feature a two-column grid layout. Added four image elements in one of the columns to improve the visual presentation of the gallery. This change enhances the user interface by providing a more organized and responsive design.

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Gallery = () => {
     return (
-        <div>
+        <div className="grid grid-cols-2">
+            <div className="grid grid-cols-2">
+                <img src="/assets/image/IMG_0582.jpg"/>
+                <img src="/assets/image/IMG_0343.jpg"/>
+                <img src="/assets/image/IMG_0559.jpg"/>
+                <img src="/assets/image/Adrian%20Dribbling.jpg"/>
+            </div>
+            <div>
 
+            </div>
         </div>
     );
 };
